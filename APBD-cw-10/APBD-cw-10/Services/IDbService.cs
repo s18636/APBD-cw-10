6b9b333a99b19254ebf0e7a2bcf0257e993a6bfb
s18636 <s18636@pjwstk.edu.pl>
2020-06-27 11:33:02 +0200

    initial commit + poczatek cwiczenia 10 zadanie 1 skonczone. rozpoczete zadanie 2

---
+++ APBD-cw-10/APBD-cw-10/Services/IDbService.cs
@@ -0,0 +1,23 @@
+﻿using APBD_cw_10.DTOs.requests;
+using APBD_cw_10.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD_cw_10.Services
+{
+    interface IDbService
+    {
+        Task<IActionResult> GetStudents();
+        Task<IActionResult> ModifyStudent(ModifyStudentRequest request);
+        
+        Task<IActionResult> RemoveStudent(RemoveStudentRequest request);
+        Task<IActionResult> PromoteStudents(PromoteStudentsRequest request);
+        Task<IActionResult> EnrollStudent(EnrollStudentRequest request);
+
+
+
+    }
+}
